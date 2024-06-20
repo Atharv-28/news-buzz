c3d20a4617fa4b72647d62d862eb16fa71eb6641
Atharv-28 <atharvmt@outlook.com>
2024-06-20 12:24:20 +0530

    Navigation UI optimized & Api key stored

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ src/components for small HTML structures
 src/features for js function to handle and process data
 
 src/pages  for pages of react app(here components will be imported)
+
+
+# API Key from newsapi.org
+ API key is: feb043f853ee48c383b367265745bca8
+ Please don't use my api key if you are copying the project as my free account have limited fetching of news. 
+
+ # Base URL
+ https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=feb043f853ee48c383b367265745bca8
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,14 +6,24 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <nav className="navigation">
-      <Link className="navbut" to="/" >Home Page</Link>
-        <Link className="navbut" to="/Technology" >Technology</Link>
-        <Link className="navbut" to="/Entertainment" >Entertainment</Link>
-        <Link className="navbut" to="/Business" >Business</Link>
-        <Link className="navbut" to="/Sports" >Sports</Link>
-        <Link className="navbut" to="/Politics" >Politics</Link>
-        <Link className="navbut" to="/Motorsports" >Motorsports</Link>
-        <Link className="navbut" to="/Games" >Games</Link>
+        <Link className="navbut" to="/">
+          Home Page
+        </Link>
+        <Link className="navbut" to="/Technology">
+          Technology
+        </Link>
+        <Link className="navbut" to="/Entertainment">
+          Entertainment
+        </Link>
+        <Link className="navbut" to="/Business">
+          Business
+        </Link>
+        <Link className="navbut" to="/Sports">
+          Sports
+        </Link>
+        <Link className="navbut" to="/Politics">
+          Politics
+        </Link>
       </nav>
 
       <Outlet />
